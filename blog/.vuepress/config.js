91f5f02606a5585c167439ff0bd974a30171d238
Clément Trempé <erpriex@gmail.com>
2022-10-01 03:06:36 +0200

    🚀 First commit

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: 'Devblog',
+
+
+    theme: '@vuepress/theme-blog',
+    themeConfig: {
+        dateFormat: 'DD-MM-YYYY',
+        nav: [
+            {
+                text: 'CheatSheet',
+                link: 'https://cheatsheet.trempe.dev',
+            },
+            {
+                text: 'Portfolio',
+                link: 'https://ctrempe.fr',
+            },
+        ],
+        footer: {
+            contact: [
+                {
+                    type: 'github',
+                    link: 'https://github.com/Erpriex',
+                },
+            ]
+        }
+    }
+
+}
